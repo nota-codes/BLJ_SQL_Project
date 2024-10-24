df7e410c3ab0ddc4e4afca74b6404115aac0375d
jonathanwirz <jonathan.wirz@icloud.com>
2024-10-24 14:38:51 +0200

    adding testing

--- testing.sql
+++ testing.sql
@@ -10,3 +10,26 @@ SELECT * FROM Assistent WHERE Vorname LIKE '_r%'
 SELECT MIN(Geburtsdatum) FROM Assistent
 
 SELECT COUNT(Matrikelnummer) FROM Student WHERE Matrikelnummer LIKE '13-%'
+
+SELECT Vorname, Nachname FROM Student WHERE Vorname LIKE '%a%' ORDER BY Nachname DESC;
+
+SELECT COUNT(Note) FROM Prüfung WHERE Note IS NOT NULL
+
+SELECT COUNT(VS.Matrikelnummer) AS Anzahl_Studenten
+FROM Vorlesung V
+JOIN VorlesungXStudent VS ON V.VorlesungID = VS.VorlesungID
+WHERE V.Titel = 'Kinetik';
+
+SELECT COUNT(Prüfung.PrüfungID) AS Anzahl_Prüfungen
+FROM Prüfung
+JOIN Professor ON Prüfung.ProfessorID = Professor.ProfessorID
+WHERE Professor.Nachname = 'van ''t Hoff';
+
+SELECT TOP 1 Vorname
+FROM Assistent
+ORDER BY Geburtsdatum ASC;
+
+
+
+
+
